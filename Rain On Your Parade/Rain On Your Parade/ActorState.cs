d5bb880e1d181a6cf47810787954764b930fff12
Athena <athena.ftw@hotmail.com>
2013-10-05 17:06:33 -0400

    Added WorldObject and ObjectType classes

--- Rain On Your Parade/Rain On Your Parade/ActorState.cs
+++ Rain On Your Parade/Rain On Your Parade/ActorState.cs
@@ -4,8 +4,23 @@ namespace Rain_On_Your_Parade
 {
     public class ActorState
     {
-        public ActorState()
+
+        enum ActorState { Sleep, Play, Nurture, Rampage };
+        private ActorState state;
+
+        public ActorState(ActorState aState)
         {
+            state = aState;
+        }
+
+        public ActorState getActorState()
+        {
+            return state;
+        }
+
+        public void setActorState(ActorState newState)
+        {
+            state = newState;
         }
     }
 }
---
+++ Rain On Your Parade/Rain On Your Parade/WorldObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rain_On_Your_Parade
+{
+    public class WorldObject
+    {
+        private ObjectType type;
+        private bool activated;
+
+        public WorldObject(ObjectType oType)
+        {
+            type = oType;
+            activated = oType.getStartsActivated();
+        }
+
+        public bool getActivated()
+        {
+            return activated;
+        }
+
+        public void activate()
+        {
+            activated = true;
+        }
+
+        public void deactivate()
+        {
+            activated = false;
+        }
+    }
+}
