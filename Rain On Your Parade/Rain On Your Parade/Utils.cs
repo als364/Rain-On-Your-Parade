5df5feea68e9a858921fac141390795a7a543474
als364 <als364@cornell.edu>
2013-10-06 13:36:19 -0400

    Conforms to C# style and contains preference heuristic for AI

---
+++ Rain On Your Parade/Rain On Your Parade/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Rain_On_Your_Parade
+{
+    static class Utils
+    {
+        public static double EuclideanDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
+        }
+    }
+}
