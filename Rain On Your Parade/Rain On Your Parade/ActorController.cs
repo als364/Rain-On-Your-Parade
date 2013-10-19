9cd3cd10525b267c1a9d961675a52b635434865c
als364 <als364@cornell.edu>
2013-10-19 10:46:52 -0400

    commented actorController

--- Rain On Your Parade/Rain On Your Parade/ActorController.cs
+++ Rain On Your Parade/Rain On Your Parade/ActorController.cs
@@ -15,65 +15,83 @@ namespace Rain_On_Your_Parade
         {
             controlledActor = actor;
         }
-
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="worldState"></param>
         public override void Update(GameTime gameTime, WorldState worldState)
         {
             //Console.WriteLine("State: " + controlledActor.State.State);
             switch (controlledActor.State.State)
             {
-                /*case ActorState.AState.Nurture:
+                //TODO: Implement these four states
+                case ActorState.AState.Nurture:
                     break;
                 case ActorState.AState.Play:
                     break;
                 case ActorState.AState.Rampage:
                     break;
                 case ActorState.AState.Sleep:
-                    break;*/
+                    break;
                 case ActorState.AState.Seek:
+                    //Actor figures out what state it wants to be 
                     ActorState.AState newState = DetermineTargetState();
                     controlledActor.TargetState = newState;
-                    //Console.WriteLine("Position: " + controlledActor.Position);
-                    //Console.WriteLine("World Size: [" + worldState.worldWidth + " x " + worldState.worldHeight + "]");
+                    //PreferenceSearch determines the most desired square.
+                    //FindPath finds a path to it.
                     controlledActor.Path = FindPath(PreferenceSearch(worldState),
                                                         worldState.StateOfWorld[(int)(controlledActor.Position.X/Canvas.SQUARE_SIZE), (int)(controlledActor.Position.Y/Canvas.SQUARE_SIZE)],
                                                         worldState.StateOfWorld, new Point[worldState.worldWidth, worldState.worldHeight]);
+                    //Now, walk there.
                     controlledActor.State = new ActorState(ActorState.AState.Walk);
                     break;
                 case ActorState.AState.Walk:
+                    //Shouldn't ever happen, really. This is a specialcase. Rather than throwing an error, just find something else to do.
                     if (controlledActor.Path.Count == 0)
                     {
                         //Console.WriteLine("Path empty, moving to Seek");
                         controlledActor.State = new ActorState(ActorState.AState.Seek);
                     }
+                    //At target square. Move to target state.
                     else if (controlledActor.Path.Count == 1)
                     {
                         //Console.WriteLine("Path ended, moving to " + controlledActor.TargetState);
+                        controlledActor.Path.Clear();
                         controlledActor.State = new ActorState(controlledActor.TargetState);
                     }
+                    //The actual moving along the path.
                     else
                     {
                         Console.WriteLine("Moving along path");
                         Console.WriteLine("Current Square: " + controlledActor.GridSquareLocation());
                         GridSquare nextSquare = controlledActor.Path[0];
                         Console.WriteLine("Next Square: " + nextSquare.Location);
+                        //If I'm within the next square on the path, remove it from the path and set my velocity towards the next one
                         if (nextSquare.Contains(controlledActor.Position))
                         {
                             controlledActor.Path.RemoveAt(0);
                             nextSquare = controlledActor.Path[0];
-                            controlledActor.Velocity = new Vector2(nextSquare.Location.X - controlledActor.Position.X, nextSquare.Location.Y - controlledActor.Position.Y);
+                            controlledActor.Velocity = new Vector2(nextSquare.Location.X - (controlledActor.Position.X/Canvas.SQUARE_SIZE), nextSquare.Location.Y - (controlledActor.Position.Y/Canvas.SQUARE_SIZE));
                             Console.WriteLine("Velocity: " + controlledActor.Velocity);
                         }
+                        //If I'm not within the next square on the path, make sure my velocity is set correctly (necessary for first square)
                         else
                         {
                             controlledActor.Velocity = new Vector2(nextSquare.Location.X - controlledActor.Position.X, nextSquare.Location.Y - controlledActor.Position.Y);
                         }
-                        //controlledActor.Velocity.Normalize();
-                        //controlledActor.Position = Vector2.Add(controlledActor.Position, controlledActor.Velocity);
+                        //Otherwise they'll just jump there
+                        controlledActor.Velocity.Normalize();
+                        //Move the actor
+                        controlledActor.Position = Vector2.Add(controlledActor.Position, controlledActor.Velocity);
                     }
                     break;
+                //Again, a specialcase. Just dumps things into Seek, making sure to zero their velocity first.
                 default:
                     if (controlledActor.Path.Count == 0)
                     {
+                        controlledActor.Velocity = new Vector2();
                         controlledActor.State = new ActorState(ActorState.AState.Seek);
                     }
                     else if (controlledActor.Path.Count == 1) //at target!
@@ -91,11 +109,20 @@ namespace Rain_On_Your_Parade
             }
         }
 
+        /// <summary>
+        /// Determines what state the controlled actor should be in based on the attribute levels of that actor.
+        /// </summary>
+        /// <returns>The new target state of the controlled actor.</returns>
         private ActorState.AState DetermineTargetState()
         {
-            ActorState.AState currentState = controlledActor.State.State;
+            //Keeps track of whatever the max attribute level we've seen so far is. This will eventually turn into something more stochastic.
             int maxLevel = 0;
             List<ActorState.AState> highestNeeds = new List<ActorState.AState>();
+
+            //For each of the attributes, keep track of whether they exceed the max attribute seen so far.
+            //If so, clear the highestNeeds list and add that one.
+            //If it's just equal, append that need.
+            //If it's strictly less, ignore it.
             if (controlledActor.Mood > maxLevel)
             {
                 maxLevel = controlledActor.Mood;
@@ -106,6 +133,7 @@ namespace Rain_On_Your_Parade
             {
                 highestNeeds.Add(ActorState.AState.Rampage);
             }
+
             if (controlledActor.NurtureLevel > maxLevel)
             {
                 maxLevel = controlledActor.NurtureLevel;
@@ -116,6 +144,7 @@ namespace Rain_On_Your_Parade
             {
                 highestNeeds.Add(ActorState.AState.Nurture);
             }
+
             if (controlledActor.PlayLevel > maxLevel)
             {
                 maxLevel = controlledActor.PlayLevel;
@@ -126,6 +155,7 @@ namespace Rain_On_Your_Parade
             {
                 highestNeeds.Add(ActorState.AState.Play);
             }
+
             if (controlledActor.SleepLevel > maxLevel)
             {
                 maxLevel = controlledActor.SleepLevel;
@@ -136,27 +166,37 @@ namespace Rain_On_Your_Parade
             {
                 highestNeeds.Add(ActorState.AState.Sleep);
             }
+
+            //Pick some random highest need.
             Random random = new Random();
             int index = random.Next(highestNeeds.Count);
             return highestNeeds[index];
         }
 
+        /// <summary>
+        /// Figure out what grid square(s) are best suited for the controlled actor
+        /// </summary>
+        /// <param name="worldState">The state of the game world</param>
+        /// <returns>A list of equally suitable grid squares</returns>
         private List<GridSquare> PreferenceSearch(WorldState worldState)
         {
             double maxPreference = 0;
             List<GridSquare> targets = new List<GridSquare>();
             foreach (GridSquare square in worldState.StateOfWorld)
             {
-                double desirability = Desirability(square, controlledActor.Position);
+                //How desirable /is/ the square
+                double desirability = Desirability(square);
                 Console.WriteLine("GridSquare: " + square.Location);
                 Console.WriteLine("Desirability: " + desirability);
                 Console.WriteLine("MaxPreference: " + maxPreference);
+                //If it's more desirable than anything else we've seen, clear the targets list and add that square
                 if (desirability > maxPreference)
                 {
                     targets.Clear();
                     targets.Add(square);
                     maxPreference = desirability;
                 }
+                //If it's equally desirable, add it to the list
                 else if (desirability == maxPreference)
                 {
                     targets.Add(square);
@@ -165,21 +205,35 @@ namespace Rain_On_Your_Parade
             return targets;
         }
 
-        private double Desirability(GridSquare target, Vector2 currentLocation)
+        /// <summary>
+        /// A heuristic for determining how desirable a square is to the controlled actor.
+        /// </summary>
+        /// <param name="target">The square whose desirability we're measuring.</param>
+        /// <returns>The desirability value of the target square.</returns>
+        private double Desirability(GridSquare target)
         {
             double desirability = 0;
             if (!target.IsPassable)
             {
                 return 0;
             }
+            //This heuristic is broken. We should only look for squares suitable for the target action.
             desirability = (target.TotalNurture * controlledActor.NurtureLevel) + 
                            (target.TotalPlay * controlledActor.PlayLevel) + 
                            (target.TotalRampage * controlledActor.Mood) + 
                            (target.TotalSleep * controlledActor.SleepLevel);
-            desirability /= Utils.EuclideanDistance(currentLocation, target.Location);
+            desirability /= Utils.EuclideanDistance(new Vector2(controlledActor.Position.X/Canvas.SQUARE_SIZE, controlledActor.Position.Y/Canvas.SQUARE_SIZE), target.Location);
             return desirability;
         }
 
+        /// <summary>
+        /// Breadth-first search.
+        /// </summary>
+        /// <param name="targets">Goal states.</param>
+        /// <param name="currentSquare">Starting state.</param>
+        /// <param name="worldGrid">State space.</param>
+        /// <param name="parentArray">For keeping track of the actual path. Keeps track of the progress of the BFS.</param>
+        /// <returns></returns>
         private List<GridSquare> FindPath(List<GridSquare> targets, GridSquare currentSquare, GridSquare[,] worldGrid, Point[,] parentArray)
         {
             Queue<GridSquare> queue = new Queue<GridSquare>();
@@ -187,9 +241,11 @@ namespace Rain_On_Your_Parade
             List<GridSquare> path = new List<GridSquare>();
             queue.Enqueue(currentSquare);
             seen.Add(currentSquare);
+            //As long as we haven't run out of squares...
             while (queue.Count != 0)
             {
                 GridSquare lookingAt = queue.Dequeue();
+                //When we reach a goal state, go through the parent array to find the actual path the actor should take.
                 if (targets.Contains(lookingAt))
                 {
                     List<Point> pointPath = ExtractPathFromTarget(parentArray, lookingAt.Location, currentSquare.Location);
@@ -199,6 +255,7 @@ namespace Rain_On_Your_Parade
                     }
                     return path;
                 }
+                //Otherwise keep looking...
                 else
                 {
                     foreach (GridSquare gridSquare in lookingAt.Adjacent)
@@ -215,6 +272,13 @@ namespace Rain_On_Your_Parade
             return path;
         }
 
+        /// <summary>
+        /// Follows pointers back up through the parent array from the specified target to the specified origin. Returns a path from the origin to the target.
+        /// </summary>
+        /// <param name="parentArray">An array indicating the progress of a breadth-first search.</param>
+        /// <param name="target">The goal state.</param>
+        /// <param name="origin">The starting state.</param>
+        /// <returns>A path from the starting state to the goal state.</returns>
         private List<Point> ExtractPathFromTarget(Point[,] parentArray, Point target, Point origin)
         {
             List<Point> path = new List<Point>();
