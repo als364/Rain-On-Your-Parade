33e95d369e34b2b55e1e85fe1cc8fcf800574d7e
Athena <athena.ftw@hotmail.com>
2013-10-20 22:08:45 -0400

    Fixing merge conflicts (again)

--- Rain On Your Parade/Rain On Your Parade/Canvas.cs
+++ Rain On Your Parade/Rain On Your Parade/Canvas.cs
@@ -30,9 +30,9 @@ namespace Rain_On_Your_Parade
             //Reactivate once actor pathing is fixed
             //
 
-            canvasGrid[6, 0] = new GridSquare(ActorType.Type.Cat, new Point(6, 0));
-            canvasGrid[1, 8] = new GridSquare(ActorType.Type.Kid, new Point(1, 8));
-            canvasGrid[6, 6] = new GridSquare(ActorType.Type.Mom, new Point(6, 6));
+            //canvasGrid[6, 0] = new GridSquare(ActorType.Type.Cat, new Point(6, 0));
+            //canvasGrid[1, 8] = new GridSquare(ActorType.Type.Kid, new Point(1, 8));
+            //canvasGrid[6, 6] = new GridSquare(ActorType.Type.Mom, new Point(6, 6));
             canvasGrid[1, 1] = new GridSquare(ObjectType.Type.SunnySpot, new Point(1, 1));
             canvasGrid[4, 1] = new GridSquare(ObjectType.Type.Laundry, new Point(4, 1));
             canvasGrid[8, 1] = new GridSquare(ObjectType.Type.House, new Point(8, 1));
